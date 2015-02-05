237ad676988a62d6043103ee379024b1fd0a6b49
Artem Chepurnoy <artemchep@gmail.com>
2015-02-05 11:28:22 +0200

    Track the times it was attached to window

--- project/app/src/main/java/com/achep/headsup/HeadsUpBase.java
+++ project/app/src/main/java/com/achep/headsup/HeadsUpBase.java
@@ -499,6 +499,8 @@ public class HeadsUpBase implements
             // exit animation.
             mHolder.rootView.clearAnimation();
             detachFromWindow();
+        } else {
+            getConfig().getTriggers().incrementLaunchCount(mHolder.context, this);
         }
         mAttached = true;
 
