778fb2fdeda7f06b7e6e1509f992a69a52ac1427
Artem Chepurnoy <artemchep@gmail.com>
2015-02-07 13:09:53 +0200

    Remove the init notification
    ...cause it's not that useful on pre-Lollipop and may cause some bugs.

--- project/app/src/main/java/com/achep/acdisplay/notifications/NotificationListenerJellyBeanMR2.java
+++ project/app/src/main/java/com/achep/acdisplay/notifications/NotificationListenerJellyBeanMR2.java
@@ -18,55 +18,20 @@
  */
 package com.achep.acdisplay.notifications;
 
-import android.annotation.TargetApi;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.res.Resources;
-import android.os.Build;
-import android.os.Handler;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.annotation.NonNull;
 
-import com.achep.acdisplay.App;
 import com.achep.acdisplay.services.MediaService;
-import com.achep.headsup.R;
 
 /**
  * Created by Artem Chepurnoy on 15.01.2015.
  */
 class NotificationListenerJellyBeanMR2 extends NotificationListener {
 
-    private boolean mInitialized;
-
     @Override
     public void onListenerBind(@NonNull MediaService service) {
-        mInitialized = false;
 
-        // What is the idea of init notification?
-        // Well the main goal is to access #getActiveNotifications()
-        // what seems to be not possible without dirty and buggy
-        // workarounds.
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                MediaService service = MediaService.sService;
-                if (service == null) return;
-
-                Resources res = service.getResources();
-                Notification.Builder builder = new Notification.Builder(service)
-                        .setContentTitle(res.getString(R.string.app_name))
-                        .setContentText(res.getString(R.string.notification_init_text))
-                        .setSmallIcon(R.drawable.stat_notify)
-                        .setPriority(Notification.PRIORITY_MIN)
-                        .setAutoCancel(true);
-
-                String name = Context.NOTIFICATION_SERVICE;
-                NotificationManager nm = (NotificationManager) service.getSystemService(name);
-                nm.notify(App.ID_NOTIFY_INIT, builder.build());
-            }
-        }, 2000);
     }
 
     // Never gets called on pre-Lollipop.
@@ -76,34 +41,20 @@ class NotificationListenerJellyBeanMR2 extends NotificationListener {
     @Override
     public void onNotificationPosted(@NonNull NotificationListenerService service,
                                      @NonNull StatusBarNotification sbn) {
-        if (mInitialized || !postActiveNotifications(service)) {
-            NotificationPresenter np = NotificationPresenter.getInstance();
-            np.postNotificationFromMain(service, OpenNotification.newInstance(sbn), 0);
-        }
+        NotificationPresenter np = NotificationPresenter.getInstance();
+        np.postNotificationFromMain(service, OpenNotification.newInstance(sbn), 0);
     }
 
     @Override
     public void onNotificationRemoved(@NonNull NotificationListenerService service,
                                       @NonNull StatusBarNotification sbn) {
-        if (mInitialized || !postActiveNotifications(service)) {
-            NotificationPresenter np = NotificationPresenter.getInstance();
-            np.removeNotificationFromMain(OpenNotification.newInstance(sbn));
-        }
+        NotificationPresenter np = NotificationPresenter.getInstance();
+        np.removeNotificationFromMain(OpenNotification.newInstance(sbn));
     }
 
     @Override
     public void onListenerUnbind(@NonNull MediaService mediaService) {
         NotificationPresenter.getInstance().clearFromMain(true);
-    }
-
-    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-    private boolean postActiveNotifications(@NonNull NotificationListenerService service) {
-        StatusBarNotification[] an = service.getActiveNotifications();
-        if (an == null) return false;
-        NotificationPresenter np = NotificationPresenter.getInstance();
-        np.init(service, an);
-        return mInitialized = true;
     }
 
 }
